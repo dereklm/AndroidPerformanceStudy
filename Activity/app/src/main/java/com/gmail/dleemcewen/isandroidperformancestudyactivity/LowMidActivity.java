1df9f40e3c0af055de9d823bf0b71bb17221e3fc
Derek McEwen <dleemcewen@gmail.com>
2017-02-08 04:53:51 -0600

    creating all of the low weight and middle weight activities
    created 6 activities, also added IntelliJ files to gitignore.

---
+++ Activity/app/src/main/java/com/gmail/dleemcewen/isandroidperformancestudyactivity/LowMidActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.dleemcewen.isandroidperformancestudyactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LowMidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_low_mid);
+    }
+}
--- Activity/app/src/main/java/com/gmail/dleemcewen/isandroidperformancestudyactivity/MainActivity.java
+++ Activity/app/src/main/java/com/gmail/dleemcewen/isandroidperformancestudyactivity/MainActivity.java
@@ -1,13 +1,78 @@
 package com.gmail.dleemcewen.isandroidperformancestudyactivity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button lowLow, lowMid, lowHi, midLow, midMid, midHi, hiLow, hiMid, hiHi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lowLow = (Button) findViewById(R.id.low_low);
+        lowLow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //maybe put timer here.
+                Intent intent = new Intent(getApplicationContext(), LowLowActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        midLow = (Button) findViewById(R.id.mid_low);
+        midLow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //maybe put timer here.
+                Intent intent = new Intent(getApplicationContext(), MidLowActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        hiLow = (Button) findViewById(R.id.high_low);
+        hiLow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //maybe put timer here.
+                Intent intent = new Intent(getApplicationContext(), HiLowActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lowMid = (Button) findViewById(R.id.low_mid);
+        lowMid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //maybe put timer here.
+                Intent intent = new Intent(getApplicationContext(), LowMidActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        midMid = (Button) findViewById(R.id.low_mid);
+        midMid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //maybe put timer here.
+                Intent intent = new Intent(getApplicationContext(), MidMidActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        hiMid = (Button) findViewById(R.id.low_mid);
+        hiMid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //maybe put timer here.
+                Intent intent = new Intent(getApplicationContext(), HiMidActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
